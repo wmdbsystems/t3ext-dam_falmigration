49b251f721fa5d531d6b13e44dd49c154c1abf2d
Rupert Germann <rupert.germann@wmdb.de>
2015-03-12 14:37:41 +0100

    feature: migrate tt_news imageattext and imagetiteltext to sys_file_reference

--- Classes/Service/MigrateDamTtnewsService.php
+++ Classes/Service/MigrateDamTtnewsService.php
@@ -31,41 +31,93 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  */
 class MigrateDamTtnewsService extends AbstractService {
 
-	/**
-	 * @var array
-	 */
-	protected $fieldMapping = array(
-		'tx_damnews_dam_images' => 'tx_falttnews_fal_images',
-		'tx_damnews_dam_media' => 'tx_falttnews_fal_media'
-	);
+    /**
+     * @var array
+     */
+    protected $fieldMapping = array(
+        'tx_damnews_dam_images' => 'tx_falttnews_fal_images',
+        'tx_damnews_dam_media' => 'tx_falttnews_fal_media'
+    );
 
-	/**
-	 * Main function, returns a FlashMessge
-	 *
-	 * @throws \Exception
-	 *
-	 * @return \TYPO3\CMS\Core\Messaging\FlashMessage
-	 */
-	public function execute() {
-		$this->controller->headerMessage(LocalizationUtility::translate('migrateDamTtnewsCommand', 'dam_falmigration'));
-		if (!$this->isTableAvailable('tx_dam_mm_ref')) {
-			return $this->getResultMessage('referenceTableNotFound');
-		}
+    /**
+     * Main function, returns a FlashMessge
+     *
+     * @throws \Exception
+     *
+     * @return \TYPO3\CMS\Core\Messaging\FlashMessage
+     */
+    public function execute() {
+        $this->controller->headerMessage(LocalizationUtility::translate('migrateDamTtnewsCommand', 'dam_falmigration'));
+        if (!$this->isTableAvailable('tx_dam_mm_ref')) {
+            return $this->getResultMessage('referenceTableNotFound');
+        }
 
-		$articlesWithImagesResult = $this->getRecordsWithDamConnections('tt_news', 'tx_damnews_dam_images');
-		$this->migrateDamReferencesToFalReferences($articlesWithImagesResult, 'tt_news', 'image', $this->fieldMapping);
+        $articlesWithImagesResult = $this->getRecordsWithDamConnections('tt_news', 'tx_damnews_dam_images');
+        $this->migrateDamReferencesToFalReferences($articlesWithImagesResult, 'tt_news', 'image', $this->fieldMapping);
 
-		$articlesWithMediaResult = $this->getRecordsWithDamConnections('tt_news', 'tx_damnews_dam_media');
-		$this->migrateDamReferencesToFalReferences($articlesWithMediaResult, 'tt_news', 'media', $this->fieldMapping);
+        $articlesWithMediaResult = $this->getRecordsWithDamConnections('tt_news', 'tx_damnews_dam_media');
+        $this->migrateDamReferencesToFalReferences($articlesWithMediaResult, 'tt_news', 'media', $this->fieldMapping);
 
-		$articlesWithvditeaserResult = $this->getRecordsWithDamConnections('tt_news', 'tx_wmdbvditeaser_damimage');
-		$this->migrateDamReferencesToFalReferences($articlesWithvditeaserResult, 'tt_news', 'image');
+        $articlesWithvditeaserResult = $this->getRecordsWithDamConnections('tt_news', 'tx_wmdbvditeaser_damimage');
+        $this->migrateDamReferencesToFalReferences($articlesWithvditeaserResult, 'tt_news', 'image');
+
+        $this->updateFalFieldsFromTtNews();
+
+        $this->updateReferenceCounters('tt_news');
+
+        return $this->getResultMessage();
+    }
+
+    /**
+     * Update sys_file_reference with fields from tt_news (alttext, titletext)
+     *
+     *
+     * @return void
+     */
+    protected function updateFalFieldsFromTtNews() {
+        $rows = $this->database->exec_SELECTgetRows(
+            'ref.uid_foreign, ref.uid_local, news.imagealttext, news.imagetitletext',
+            'sys_file_reference ref
+            INNER JOIN tt_news news
+             ON ref.uid_foreign = news.uid',
+            "ref.tablenames = 'tt_news' AND (ref.fieldname = 'tx_falttnews_fal_images' OR ref.fieldname = 'tx_falttnews_fal_media')"
+        );
+
+        $updateArray = array();
+
+        foreach($rows as $row) {
+            // checks for empty values
+            if($row['imagetitletext'] !== '' && $row['alternative'] !== '') {
+                $updateArray = array(
+                    'title' => $row['imagetitletext'],
+                    'alternative' => $row['imagealttext']
+                );
+            }
+
+            if($row['imagetitletext'] === '' && $row['alternative'] !== '') {
+                $updateArray = array(
+                    'alternative' => $row['imagealttext']
+                );
+            }
+
+            if($row['imagetitletext'] !== '' && $row['alternative'] === '') {
+                $updateArray = array(
+                    'title' => $row['imagetitletext']
+                );
+            }
+
+            if($row['imagetitletext'] === '' && $row['alternative'] === '') {
+                continue;
+            }
 
 
-
-		$this->updateReferenceCounters('tt_news');
-
-		return $this->getResultMessage();
-	}
+            // update sys_file_reference
+            $this->database->exec_UPDATEquery(
+                'sys_file_reference',
+                'uid_foreign = ' . $row['uid_foreign'],
+                $updateArray
+            );
+        }
+    }
 
 }
